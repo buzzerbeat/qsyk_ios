35e81571dbcb5b0a2111a2c57f93ef67cb299b51
huiyumiao <6265423134@qq.com>
2016-04-24 09:02:24 +0800

    Initial Commit

---
+++ qingsongyike/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  qingsongyike
+//
+//  Created by 苗慧宇 on 4/24/16.
+//  Copyright © 2016 subo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ qingsongyike/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  qingsongyike
+//
+//  Created by 苗慧宇 on 4/24/16.
+//  Copyright © 2016 subo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
